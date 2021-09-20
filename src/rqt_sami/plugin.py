a3c9c850a6bd4a0c14312759213752a4a605548e
Fábio Alves <fabioalves98@ua.pt>
2021-09-20 18:08:13 +0100

    joint velocity controls added to rqt_sami

--- src/rqt_sami/plugin.py
+++ src/rqt_sami/plugin.py
@@ -1,5 +1,11 @@
-import os
+#!/usr/bin/env python
+import os, time
+from math import degrees
 import rospy, rospkg, rosparam
+from geometry_msgs.msg import Vector3
+from std_srvs.srv import Trigger
+from ur_msgs.srv import SetSpeedSliderFraction
+from controller_manager_msgs.srv import ListControllers, SwitchController
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
@@ -33,6 +39,42 @@ class SamiPlugin(Plugin):
         self.fillAliasLayout()
         self._widget.alias_send.clicked[bool].connect(self.alias_send_button_clicked)
         self._widget.alias_save.clicked[bool].connect(self.alias_save_button_clicked)
+
+        # Thread for robot velocity control       
+        self.vel_thread = QThread()
+        self.vel_worker = VelocityControl()
+        self.vel_worker.moveToThread(self.vel_thread)
+        self.vel_thread.started.connect(self.vel_worker.run)
+        self.vel_thread.start()
+
+        # Linear Axis Move
+        self._widget.x_lin_up.pressed.connect(lambda: self.velocity_move_pressed('lx+'))
+        self._widget.x_lin_up.released.connect(self.velocity_move_released)
+        self._widget.x_lin_down.pressed.connect(lambda: self.velocity_move_pressed('lx-'))
+        self._widget.x_lin_down.released.connect(self.velocity_move_released)
+        self._widget.y_lin_up.pressed.connect(lambda: self.velocity_move_pressed('ly+'))
+        self._widget.y_lin_up.released.connect(self.velocity_move_released)
+        self._widget.y_lin_down.pressed.connect(lambda: self.velocity_move_pressed('ly-'))
+        self._widget.y_lin_down.released.connect(self.velocity_move_released)
+        self._widget.z_lin_up.pressed.connect(lambda: self.velocity_move_pressed('lz+'))
+        self._widget.z_lin_up.released.connect(self.velocity_move_released)
+        self._widget.z_lin_down.pressed.connect(lambda: self.velocity_move_pressed('lz-'))
+        self._widget.z_lin_down.released.connect(self.velocity_move_released)
+
+        # Angular Axis Move
+        self._widget.x_ang_up.pressed.connect(lambda: self.velocity_move_pressed('ax+'))
+        self._widget.x_ang_up.released.connect(self.velocity_move_released)
+        self._widget.x_ang_down.pressed.connect(lambda: self.velocity_move_pressed('ax-'))
+        self._widget.x_ang_down.released.connect(self.velocity_move_released)
+        self._widget.y_ang_up.pressed.connect(lambda: self.velocity_move_pressed('ay+'))
+        self._widget.y_ang_up.released.connect(self.velocity_move_released)
+        self._widget.y_ang_down.pressed.connect(lambda: self.velocity_move_pressed('ay-'))
+        self._widget.y_ang_down.released.connect(self.velocity_move_released)
+        self._widget.z_ang_up.pressed.connect(lambda: self.velocity_move_pressed('az+'))
+        self._widget.z_ang_up.released.connect(self.velocity_move_released)
+        self._widget.z_ang_down.pressed.connect(lambda: self.velocity_move_pressed('az-'))
+        self._widget.z_ang_down.released.connect(self.velocity_move_released)
+
         # Relative move service
         self._widget.r_move.clicked[bool].connect(self.rel_move_button_clicked)
         self._widget.r_reset.clicked[bool].connect(self.rel_move_reset_button_clicked)
@@ -45,13 +87,23 @@ class SamiPlugin(Plugin):
         # Gripper services
         self._widget.grip.clicked[bool].connect(self.gripper_button_clicked)
         self._widget.release.clicked[bool].connect(self.release_button_clicked)
+        # Controller switcher
+        self._widget.switch_controller.clicked[bool].connect(self.switch_controller_button_clicked)
+        # Resend robot program
+        self._widget.resend_program.clicked[bool].connect(self.resend_program_button_clicked)
+        # Zero FT sensor
+        self._widget.zero_ft_sensor.clicked[bool].connect(self.zero_ft_sensor_button_clicked)
+        # Set Speed Slider
+        self._widget.set_speed_slider.clicked[bool].connect(self.set_speed_slider_button_clicked)
+
+
         # Status thread for robot information       
-        self.thread = QThread()
-        self.worker = StatusDialog()
-        self.worker.moveToThread(self.thread)
-        self.worker.status_signal.connect(self.status_update)
-        self.thread.started.connect(self.worker.get_status)
-        self.thread.start()
+        self.status_thread = QThread()
+        self.status_worker = StatusDialog()
+        self.status_worker.moveToThread(self.status_thread)
+        self.status_worker.status_signal.connect(self.status_update)
+        self.status_thread.started.connect(self.status_worker.get_status)
+        self.status_thread.start()
 
         # Show _widget.windowTitle on left-top of each plugin (when 
         # it's set in _widget). This is useful when you open multiple 
@@ -109,6 +161,14 @@ class SamiPlugin(Plugin):
     def alias_save_button_clicked(self):
         # TODO implement save alias function
         pass
+
+
+    def velocity_move_pressed(self, velocity):
+        self.vel_worker.activate(velocity)
+
+
+    def velocity_move_released(self):
+        self.vel_worker.deactivate()
 
 
     def status_update(self, data):
@@ -207,6 +267,15 @@ class SamiPlugin(Plugin):
         self._widget.j_w_1.clear()
         self._widget.j_w_2.clear()
         self._widget.j_w_3.clear()
+
+        joints = self.status_worker.joints
+        
+        self._widget.j_s_p.setText(str(joints[0]))
+        self._widget.j_s_l.setText(str(joints[1]))
+        self._widget.j_elb.setText(str(joints[2]))
+        self._widget.j_w_1.setText(str(joints[3]))
+        self._widget.j_w_2.setText(str(joints[4]))
+        self._widget.j_w_3.setText(str(joints[5]))
     
 
     def gripper_button_clicked(self):
@@ -220,6 +289,7 @@ class SamiPlugin(Plugin):
 
         self._widget.com_response.setPlainText(resp)
 
+
     def release_button_clicked(self):
         # Release service caller
         rospy.wait_for_service('iris_sami/release')
@@ -231,10 +301,146 @@ class SamiPlugin(Plugin):
 
         self._widget.com_response.setPlainText(resp)
 
+
+    def switch_controller_button_clicked(self):
+        controllers = ['joint_group_vel_controller', 'scaled_pos_joint_traj_controller']
+        status = ''
+
+        # Get active controller
+        active_controller = ''
+        rospy.wait_for_service('controller_manager/list_controllers')
+        try:
+            listServ = rospy.ServiceProxy('controller_manager/list_controllers', ListControllers)
+            resp = listServ()
+            for controller in resp.controller:
+                if controller.name in controllers and controller.state == "running":
+                    active_controller = controller.name
+        except rospy.ServiceException as e:
+            status = "Service call failed: %s" % e
+
+        # Switch Controllers
+        if active_controller:
+            controllers.remove(active_controller)
+
+            rospy.wait_for_service('controller_manager/switch_controller')
+            try:
+                switchServ = rospy.ServiceProxy('controller_manager/switch_controller', SwitchController)
+                resp = switchServ(start_controllers=controllers,
+                                stop_controllers=[active_controller],
+                                strictness=1,
+                                start_asap=False,
+                                timeout=2.0)
+                status = "Switched Controllers: " + controllers[0] + " is active"
+            except rospy.ServiceException as e:
+                status = "Service call failed: %s" % e
+        else:
+            status = "No controller is active. There might be a problem with the driver"
+        
+        self._widget.com_response.setPlainText(status)
+
+
+    def resend_program_button_clicked(self):
+        try:
+            rospy.wait_for_service('/ur_hardware_interface/resend_robot_program', timeout=1)
+            resend = rospy.ServiceProxy('/ur_hardware_interface/resend_robot_program', Trigger)
+            if resend().success:
+                resp = "Program sucessfyly sent to robot"
+            else:
+                resp = "Error un resending program to robot"
+        except (rospy.ServiceException,rospy.exceptions.ROSException) as e:
+            resp = "Service call failed: %s" % e
+
+        self._widget.com_response.setPlainText(resp)
+
+
+    def zero_ft_sensor_button_clicked(self):
+        try:
+            rospy.wait_for_service('/ur_hardware_interface/zero_ftsensor', timeout=1)
+            zero = rospy.ServiceProxy('/ur_hardware_interface/zero_ftsensor', Trigger)
+            if zero().success:
+                resp = "FT Sensor sucessfully tared"
+            else:
+                resp = "Error in taring FT Sensor"
+        except (rospy.ServiceException,rospy.exceptions.ROSException) as e:
+            resp = "Service call failed: %s" % e
+
+        self._widget.com_response.setPlainText(resp)
+
+
+    def set_speed_slider_button_clicked(self):
+        speed_slider = self._widget.speed_slider.text()
+        speed_slider = float(speed_slider)
+        
+        rospy.wait_for_service('/ur_hardware_interface/set_speed_slider')
+        try:
+            set_speed = rospy.ServiceProxy('/ur_hardware_interface/set_speed_slider', SetSpeedSliderFraction)
+            resp = set_speed(speed_slider)
+            resp = resp.success
+        except rospy.ServiceException as e:
+            resp = "Service call failed: %s" % e
+
+
+
+
     #def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure
         # This will enable a setting button (gear icon) in each dock widget title bar
         # Usually used to open a modal configuration dialog
+
+
+class VelocityControl(QObject):
+    axis_to_vel = {
+        's' : [0, 0, 0],
+        'lx+' : [1, 0, 0],
+        'ly+' : [0, 1, 0],
+        'lz+' : [0, 0, 1],
+        'lx-' : [-1, 0, 0],
+        'ly-' : [0, -1, 0],
+        'lz-' : [0, 0, -1],
+        'ax+' : [0.5, 0, 0],
+        'ay+' : [0, 0.5, 0],
+        'az+' : [0, 0, 0.5],
+        'ax-' : [-0.5, 0, 0],
+        'ay-' : [0, -0.5, 0],
+        'az-' : [0, 0, -0.5],
+    }
+
+    def __init__(self):
+        super(VelocityControl, self).__init__()
+
+        self.active = False
+        self.velocity = 's'
+
+        self.lin_vel_publisher = rospy.Publisher('/linear_velocity', Vector3, queue_size=1)
+        self.ang_vel_publisher = rospy.Publisher('/angular_velocity', Vector3, queue_size=1)
+        
+
+    def activate(self, velocity):
+        self.velocity = velocity
+        self.active = True
+
+
+    def deactivate(self):
+        self.velocity = 's'
+        time.sleep(0.1)
+        self.active = False
+
+
+    def run(self):
+        rate = rospy.Rate(500)
+
+        while not rospy.is_shutdown():
+            if self.active:
+                vel_msg = Vector3(*self.axis_to_vel[self.velocity])
+                if self.velocity[0] == 'l':
+                    self.lin_vel_publisher.publish(vel_msg)
+                elif self.velocity[0] == 'a':
+                    self.ang_vel_publisher.publish(vel_msg)
+                else:
+                    self.lin_vel_publisher.publish(vel_msg)
+                    self.ang_vel_publisher.publish(vel_msg)
+            
+            rate.sleep()
 
 
 class StatusDialog(QObject):
@@ -255,9 +461,11 @@ class StatusDialog(QObject):
     def status_subscriber(self, data):
         if data.success:
             self.status = data.feedback
-            self.joints = [round(x, 5) for x in data.joints]
-            self.position = data.pose.position
-            self.orientation = data.pose.orientation
+            self.joints = [round(x, 3) for x in data.joints]
+            pos = data.pose.position
+            self.position = [round(x, 5) for x in [pos.x, pos.y, pos.z]]
+            ori = data.pose.orientation
+            self.orientation = [round(x, 5) for x in [ori.x, ori.y, ori.z, ori.w]]
             self.velocity = data.velocity
     
 
@@ -266,14 +474,17 @@ class StatusDialog(QObject):
         rate = rospy.Rate(10)
 
         while not rospy.is_shutdown():
+            
+
+            joints_str = '[' + ', '.join(['%.3f (<b>%.1f</b>)' % (x, degrees(x)) for x in self.joints]) + ']'
 
             status_text = ("<html> \
                             <b>Status:</b> %s<br>\
                             <b>Joints:</b> %s<br>\
                             <b>Position:</b> %s<br>\
                             <b>Orientation:</b> %s<br>\
-                            <b>Velocity:</b> %f \
-                            </html>" % (self.status, str(self.joints), str(self.position), 
+                            <b>Velocity:</b> %.1f \
+                            </html>" % (self.status, joints_str, str(self.position), 
                                         str(self.orientation), self.velocity))
             
             self.status_signal.emit(status_text)
